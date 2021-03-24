bf306d3aa7ce1c9786aeb952f27f7a805641e441
Yuri Siman <yurisimannogueira@gmail.com>
2021-03-24 14:48:33 -0300

    Gerando Migrations

---
+++ src/mvc/BuracoNasLetras.Data/Mappings/PapelMapping.cs
@@ -0,0 +1,19 @@
+﻿using BuracoNasLetras.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BuracoNasLetras.Data.Mappings
+{
+    public class PapelMapping : IEntityTypeConfiguration<Papel>
+    {
+        public void Configure(EntityTypeBuilder<Papel> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Texto)
+                .IsRequired();
+
+            builder.ToTable("Papeis");
+        }
+    }
+}
---
+++ src/mvc/BuracoNasLetras.Data/Migrations/20210324174717_Initial.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BuracoNasLetras.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Papeis",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Texto = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tamanho = table.Column<int>(type: "int", nullable: false),
+                    TotalDePalavras = table.Column<int>(type: "int", nullable: false),
+                    TotalDeBuracos = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Papeis", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Papeis");
+        }
+    }
+}
